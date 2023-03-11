c7aadd5b3fd40647fb1e2adf8f45091c1664d8f8
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2023-03-11 19:42:30 +0100

    website style

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,16 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.querySelector('.nav-menu');
+// hamburger menu in mobile browsers
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
-menuToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
 });
+
+// Create display
+
+const headerSection = document.getElementById("header");
+const mainSection = document.getElementById("main");
+const aboutSection = document.getElementById("about");
+const opinionsSection = document.getElementById("opinions");
+const socialSection = document.getElementById("social");
+const lessonsSection = document.getElementById("lessons");
