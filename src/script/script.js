0d528ecb312e0c73c8673d007ffde24f87fd3948
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2023-03-14 11:43:03 +0100

    img slider

--- src/script/script.js
+++ src/script/script.js
@@ -5,3 +5,26 @@ const navMenu = document.querySelector(".nav-menu");
 menuToggle.addEventListener("click", () => {
   navMenu.classList.toggle("show");
 });
+
+// opinions slider
+const numImg = document.querySelectorAll("img").length;
+let currImg = 1;
+
+let timeoutID;
+const imgcontainer = document.querySelector(".slider");
+
+function updateImage() {
+  if (currImg > numImg) {
+    currImg = 1;
+  } else if (currImg < 1) {
+    currImg = numImg;
+  }
+  imgcontainer.style.transform = `translateX(-${(currImg - 1) * 800}px)`;
+
+  timeoutID = setTimeout(() => {
+    currImg++;
+    updateImage();
+  }, 5000);
+}
+
+updateImage();
